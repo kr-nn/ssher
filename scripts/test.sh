cee8e2f70afe15c217e030758045c8fff6f51a3b
kyle <kyle@nocturnalnerd.xyz>
2025-10-26 18:46:41 -0400

    updated tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -19,104 +19,110 @@ test_ping(){
 
 test_run(){
   echo "RUN: ==============================================="
-  echo "TEST: Run mode: ./ssher -m run -i ./_inv/test.inv -c 'nixos-version'"
-  ./ssher -m run -i ./_inv/test.inv -c 'nixos-version'
+  echo "TEST: Run mode: ./ssher run -i ./_inv/test.inv -c 'nixos-version'"
+  ./ssher run -i ./_inv/test.inv -c 'nixos-version'
   echo "RUN FAIL: ==============================================="
-  echo "TEST: Run mode: ./ssher -m run -i ./_inv/test.inv"
-  ./ssher -m run -i ./_inv/test.inv
+  echo "TEST: Run mode: ./ssher run -i ./_inv/test.inv"
+  ./ssher run -i ./_inv/test.inv
 }
 
 test_script(){
   echo "SCRIPT: ==============================================="
-  echo "TEST: script mode: ./ssher -m script -i _inv/test.inv -l ./scripts/nixos-version"
-  ./ssher -m script -i _inv/test.inv -l ./scripts/nixos-version
+  echo "TEST: script mode: ./ssher script -i _inv/test.inv -l ./scripts/nixos-version"
+  ./ssher script -i _inv/test.inv -l ./scripts/nixos-version
+
+  echo ""
   echo "SCRIPT FAIL (file doesn't exist): ====================="
-  ./ssher -m script -i _inv/test.inv -l ./scripts/asdf
+  ./ssher script -i _inv/test.inv -l ./scripts/asdf
+
+  echo ""
+  echo "SCRIPT FAIL (file not declared): ====================="
+  ./ssher script -i _inv/test.inv
 }
 
 test_upload(){
   echo "UPLOAD: ==============================================="
-  echo "TEST: upload mode: ./ssher -m upload -i _inv/test.inv -l _inv/test.inv -r inv"
-  ./ssher -m upload -i _inv/test.inv -l _inv/test.inv -r inv
+  echo "TEST: upload mode: ./ssher upload -i _inv/test.inv -l _inv/test.inv -r inv"
+  ./ssher upload -i _inv/test.inv -l _inv/test.inv -r inv
   echo "Checking uploaded file... ============================="
-  ./ssher -m run -i _inv/test.inv -c 'cat inv'
+  ./ssher run -i _inv/test.inv -c 'cat inv'
   echo "Attempting append... =================================="
-  ./ssher -m upload -i _inv/test.inv -l _inv/test.inv -r inv
+  ./ssher upload -i _inv/test.inv -l _inv/test.inv -r inv
   echo "Checking uploaded file... ============================="
-  ./ssher -m run -i _inv/test.inv -c 'cat inv'
+  ./ssher run -i _inv/test.inv -c 'cat inv'
   echo "Attempting overwrite... ==============================="
-  ./ssher -m upload -i _inv/test.inv -l _inv/test.inv -r inv
+  ./ssher upload -i _inv/test.inv -l _inv/test.inv -r inv
   echo "Checking uploaded file... ============================="
-  ./ssher -m run -i _inv/test.inv -c 'cat inv'
+  ./ssher run -i _inv/test.inv -c 'cat inv'
   echo "Cleaning up... ========================================"
-  ./ssher -m run -i _inv/test.inv -c 'rm inv'
+  ./ssher run -i _inv/test.inv -c 'rm inv'
 
   echo "UPLOAD FAIL (remote not defined): ====================="
-  ./ssher -m upload -i _inv/test.inv -l _inv/asdf.inv
+  ./ssher upload -i _inv/test.inv -l _inv/asdf.inv
   echo "UPLOAD FAIL (local not defined): ======================"
-  ./ssher -m upload -i _inv/test.inv -r inv
+  ./ssher upload -i _inv/test.inv -r inv
   echo "UPLOAD FAIL (both not defined): ======================="
-  ./ssher -m upload -i _inv/test.inv
+  ./ssher upload -i _inv/test.inv
   echo "UPLOAD FAIL (local doesn't exist): ===================="
-  ./ssher -m upload -i _inv/test.inv -l ./asdf -r inv
+  ./ssher upload -i _inv/test.inv -l ./asdf -r inv
 }
 
 test_collect(){
   echo "COLLECT: =============================================="
   echo "Seeding file =========================================="
-  ./ssher -m upload -i _inv/test.inv -l _inv/test.inv -r inv
+  ./ssher upload -i _inv/test.inv -l _inv/test.inv -r inv
 
-  echo "TEST: Collect mode: ./ssher -m collect -i _inv/test.inv -l inv -r inv"
-  ./ssher -m collect -i _inv/test.inv -l inv -r inv
+  echo "TEST: Collect mode: ./ssher collect -i _inv/test.inv -l inv -r inv"
+  ./ssher collect -i _inv/test.inv -l inv -r inv
   echo "Checking local file... ============================="
   cat inv
   echo "Cleaning up... ========================================"
   rm inv
-  ./ssher -m run -i _inv/test.inv -c 'rm inv'
+  ./ssher run -i _inv/test.inv -c 'rm inv'
 
   echo "COLLECT FAIL (remote not defined): ====================="
-  ./ssher -m collect -i _inv/test.inv -l _inv/asdf.inv
+  ./ssher collect -i _inv/test.inv -l _inv/asdf.inv
   echo "COLLECT FAIL (local not defined): ======================"
-  ./ssher -m collect -i _inv/test.inv -r inv
+  ./ssher collect -i _inv/test.inv -r inv
   echo "COLLECT FAIL (both not defined): ======================="
-  ./ssher -m collect -i _inv/test.inv
+  ./ssher collect -i _inv/test.inv
   echo "COLLECT FAIL (local doesn't exist): ===================="
-  ./ssher -m collect -i _inv/test.inv -l ./asdf -r inv
+  ./ssher collect -i _inv/test.inv -l ./asdf -r inv
 }
 
 test_update(){
   echo "UPDATE: ==============================================="
   echo "Seeding file =========================================="
-  ./ssher -m upload -i _inv/test.inv -l _inv/test.inv -r inv
-  ./ssher -m collect -i _inv/test.inv -l inv -r inv
+  ./ssher upload -i _inv/test.inv -l _inv/test.inv -r inv
+  ./ssher collect -i _inv/test.inv -l inv -r inv
   echo "Update this file so all servers have 1 item and remove this line" >> inv
   vim inv
 
-  echo "TEST: Update mode: ./ssher -m update -i _inv/test.inv -l inv -r inv"
-  ./ssher -m update -i _inv/test.inv -l inv -r inv
+  echo "TEST: Update mode: ./ssher update -i _inv/test.inv -l inv -r inv"
+  ./ssher update -i _inv/test.inv -l inv -r inv
   echo "Checking uploaded file... ============================="
-  ./ssher -m run -i _inv/test.inv -c 'cat inv'
+  ./ssher run -i _inv/test.inv -c 'cat inv'
   echo "Cleaning up... ========================================"
-  ./ssher -m run -i _inv/test.inv -c 'rm inv'
+  ./ssher run -i _inv/test.inv -c 'rm inv'
   echo "upload file if not exists ============================="
-  ./ssher -m update -i _inv/test.inv -l inv -r inv
+  ./ssher update -i _inv/test.inv -l inv -r inv
   echo "final Cleaning up... ==================================="
-  ./ssher -m run -i _inv/test.inv -c 'rm inv'
+  ./ssher run -i _inv/test.inv -c 'rm inv'
   rm inv
 
   echo "UPDATE FAIL (remote not defined): ====================="
-  ./ssher -m update -i _inv/test.inv -l _inv/asdf.inv
+  ./ssher update -i _inv/test.inv -l _inv/asdf.inv
   echo "UPDATE FAIL (local not defined): ======================"
-  ./ssher -m update -i _inv/test.inv -r inv
+  ./ssher update -i _inv/test.inv -r inv
   echo "UPDATE FAIL (both not defined): ======================="
-  ./ssher -m update -i _inv/test.inv
+  ./ssher update -i _inv/test.inv
   echo "UPDATE FAIL (local doesn't exist): ===================="
-  ./ssher -m update -i _inv/test.inv -l ./asdf -r inv
+  ./ssher update -i _inv/test.inv -l ./asdf -r inv
 }
 
 test_manual(){
   echo "MANUAL: ==============================================="
-  ./ssher -m man -i ./_inv/test.inv
+  ./ssher man -i ./_inv/test.inv
 }
 
 
@@ -125,9 +131,9 @@ test_expected_fails(){
   echo "TEST: no mode ========================================="
   ./ssher -i ./_inv/test.inv
   echo "TEST: no inventory ===================================="
-  ./ssher -m ping
+  ./ssher ping
   echo "TEST: inv doens't exist ==============================="
-  ./ssher -m ping -i ./_inv/asdf.inv
+  ./ssher ping -i ./_inv/asdf.inv
 }
 
 case "$mode" in
